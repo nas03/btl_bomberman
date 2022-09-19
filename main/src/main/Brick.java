b7d87f1abc97727e3712f852d11886282b29a325
Nguyễn Anh Sơn <91643074+nas03@users.noreply.github.com>
2022-09-19 16:21:30 +0700

    update to rebase (#4)
    * create map structure
    
    * update to rebase
    
    * update .gitignore
    
    * reconstruct game project
    
    * update to rebase
    
    * reconstruct

--- main/src/main/Brick.java
+++ main/src/main/Brick.java
@@ -1,6 +1,7 @@
 package main;
 
 public class Brick {
+
   Brick(int x, int y) {
     //brick image at (x,y)
   }
---
+++ main/src/main/Entity.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Entity {
+
+  int speed;
+
+  int getImageHeight() {
+    return 0;
+  }
+
+  int getImageWidth() {
+    return 0;
+  }
+
+  boolean checkCollision() {
+    return true;
+  }
+}
