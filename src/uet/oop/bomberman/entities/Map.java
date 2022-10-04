03947be3aefaef4a57892aca5ab741651c77dc03
Nguyễn Anh Sơn <91643074+nas03@users.noreply.github.com>
2022-10-04 16:20:33 +0700

    update bomber movement

--- src/uet/oop/bomberman/entities/Map.java
+++ src/uet/oop/bomberman/entities/Map.java
@@ -12,9 +12,9 @@ public class Map {
    * 1: wall 0: grass 2: brick
    */
 
-  int mapIndex = 0;
-  String MAP = "################################p     ** *  1 * 2 *  * * *   ## # # #*# # #*#*# # # #*#*#*# ##  x*     ***  *  1   * 2 * * ## # # # # #*# # #*#*# # # # #*##f         x **  *  *   1     ## # # # # # # # # #*# #*# # # ##*  *      *  *      *        ## # # # #*# # # #*#*# # # # # ##*    **  *       *           ## #*# # # # # # #*# # # # # # ##           *   *  *          ################################";
-  char[] mapArray = MAP.toCharArray();
+  public int mapIndex = 0;
+  public String MAP = "################################p     ** *  1 * 2 *  * * *   ## # # #*# # #*#*# # # #*#*#*# ##  x*     ***  *  1   * 2 * * ## # # # # #*# # #*#*# # # # #*##f         x **  *  *   1     ## # # # # # # # # #*# #*# # # ##*  *      *  *      *        ## # # # #*# # # #*#*# # # # # ##*    **  *       *           ## #*# # # # # # #*# # # # # # ##           *   *  *          ################################";
+  public char[] mapArray = MAP.toCharArray();
 
   public Map() {
     for (int i = 0; i < 13; i++) {
@@ -23,50 +23,11 @@ public class Map {
       }
     }
   }
-
-  public String getEntity(int x, int y) {
-    if (map[x][y] == ' ') {
-      return "Grass";
-    }
-    if (map[x][y]=='#') {
-      return "Wall";
-    }
-    if (map[x][y]=='x') {
-      return "Portal";
-    }
-    if (map[x][y]=='b') {
-      return "BombItem";
-    }
-    if (map[x][y]== 'f') {
-      return "FlameItem";
-    }
-    if (map[x][y]== 's') {
-      return "SpeedItem";
-    }
-    if (map[x][y]== '*') {
-      return "Brick";
-    }
-    return "?";
+  public char[][] getMap() {
+    return map;
   }
 
-  void updateMap() {
-    char[][] temp = new char[13][31];
-    for (int i = 0; i < 13; i++) {
-      for (int j = 0; j < 31; j++) {
-        temp[i][j] = map[i][j];
-      }
-    }
-    map = temp;
-  }
-  public static void main(String[] args){
-    Map map = new Map();
-    for (int i = 0; i < 13; i++) {
-      for (int j = 0; j < 31; j++) {
-        System.out.println(map.getEntity(i,j));
-      }
-      System.out.println("");
-    }
-  }
+
   void renderMap() {
     //Load image
   }
---
+++ src/uet/oop/bomberman/entities/enemy/Balloon.java
@@ -0,0 +1,13 @@
+package uet.oop.bomberman.entities.enemy;
+
+import  uet.oop.bomberman.entities.Entity;
+
+public class Balloon extends Entity{
+    Balloon(int x, int y, Image img) {
+        super(i);
+    }
+    @Override
+    public void update() {
+
+    }
+}
