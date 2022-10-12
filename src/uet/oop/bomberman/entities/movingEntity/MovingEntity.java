ad05b7893f4700613bf737157fce52ea27b3d299
Nguyễn Anh Sơn <91643074+nas03@users.noreply.github.com>
2022-10-12 12:29:17 +0700

    add new type of enemy with specific ability: minvo, kondoria and doll

--- src/uet/oop/bomberman/entities/movingEntity/MovingEntity.java
+++ src/uet/oop/bomberman/entities/movingEntity/MovingEntity.java
@@ -3,18 +3,17 @@ package uet.oop.bomberman.entities.movingEntity;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.*;
+import uet.oop.bomberman.graphics.Sprite;
 
 import java.util.List;
-
-
 public abstract class MovingEntity {
     protected final int WIDTH = 31;
     protected final int HEIGHT = 13;
     public boolean isAlive = true;
+    public int frame = 0;
     public boolean pressW, pressA, pressS, pressD;
     private int speed = 1;
-    public static int xPos = 1;
-    public static int yPos = 1;
+    public int xPos = 1, yPos = 1;
 
     protected Image img;
 
@@ -41,10 +40,7 @@ public abstract class MovingEntity {
         if (stillObjects.get(getObjectIndex) instanceof Brick) {
             return false;
         }
-        if (stillObjects.get(getObjectIndex) instanceof Wall) {
-            return false;
-        }
-        return x - 1 >= 0 && x + 1 <= WIDTH && y - 1 >= 0 && y + 1 <= HEIGHT;
+        return !(stillObjects.get(getObjectIndex) instanceof Wall);
     }
 
     public void left() {
@@ -76,6 +72,5 @@ public abstract class MovingEntity {
         pressS = false;
         pressD = true;
     }
-
 }
 
